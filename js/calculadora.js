c073b200b6667fd6bca01e95e92945882221136b
kaandradec <kaandradec@gmail.com>
2025-04-25 19:41:21 -0500

    taller 8: desestructuración de arreglos y objetos

--- js/calculadora.js
+++ js/calculadora.js
@@ -41,7 +41,6 @@ function evaluarOperacion(tipo) {
 
     resultadoElemento.innerText = (resultadoElemento.innerText + resultado).toString();
 
-
 }
 
 const fundamentos = () => {
@@ -137,6 +136,145 @@ const fundamentos = () => {
 
     const estudiantes = [estudiante1, estudiante2, estudiante3];
 
-    console.table(estudiantes)
+    console.table(estudiantes);
+
+    const ciudadano = {
+        nombre: 'Kevin',
+        apellido: 'Andrade',
+        direccion: {
+            callePrincipal: 'Av. America',
+            calleSecundaria: 'Av. La Gasca',
+            numeracion: '5620',
+            barrio: {
+                referencia: 'Frente al Dilipa'
+            }
+        }
+    };
+
+    console.log(ciudadano);
+    console.log(ciudadano.nombre);
+    console.log(ciudadano.direccion);
+    console.log(ciudadano.direccion.callePrincipal);
+
+    ciudadano.direccion.callePrincipal = 'Av. Amazonas';
+    console.log(ciudadano);
+
+    const arregloEstudiantes = [{ nombre: 'Thom' }, { nombre: 'Jhon' }, { nombre: 'Carla', edad: 23 }];
+    console.log(arregloEstudiantes[1].nombre);
+
+
+    // Desestructuración de Arreglos
+    const colores = ['Amarillo', 'Azul', 'Verde', 'Naranja'];
+
+    const [c1, c2, c3, c4] = colores;
+    console.log('COLOR: ', c1);
+    console.log('COLOR: ', c2);
+    console.log('COLOR: ', c3);
+    console.log('COLOR: ', c4);
+
+
+
+    const [, , es3] = arregloEstudiantes; // No es necesario obtener todos los elementos
+    console.log('ESTUDIANTE: ', es3);
+
+    const [c01, c02] = ['Amarillo', 'Azul', 'Verde', 'Naranja'];
+    console.log(c02)
+
+    // Puedo desestructurar un arreglo en cualquier parte del código
+    desestructuracionArreglo(colores);
+
+    const [, p2, p3, p4, p5, p6] = desestructuracionArreglo2();
+    console.log(p2);
+    console.log(p6);
+
+    //⚠️ La desestructuración de arreglos se hace a travez de sus posiciones
+
+    // Desestructuración por operador rest
+    console.log('>> Desestructuración por operador rest');
+    const [p1, ...resto] = desestructuracionArreglo2();
+    console.log(p1);
+    console.log(resto);
+
+    // Desestructuración de Objetos 
+    const auto = {
+        marca: 'Toyota',
+        modelo: 'Prius',
+        anio: 2024,
+        color: 'Rojo',
+    }
+    const { marca, anio } = auto;
+    console.log(marca)
+    console.log(anio)
+
+
+
+    const { anio1 } = {
+        marca1: 'Toyota',
+        modelo1: 'Prius',
+        anio1: 2024,
+    };
+
+    console.log(anio1);
+
+    desestructuracionObjeto(auto);
+
+    //
+    const universidad = {
+        nombre: 'UCE',
+        direccion: 'America',
+        rector: {
+            nombre1: 'Daniel',
+            apellido: 'Teran'
+        }
+    }
+
+    const { rector } = universidad;
+    console.log(rector);
+    const { nombre1, apellido } = rector;
+    console.log(apellido);
+    console.log(nombre1);
+
+    const universidad2 = {
+        nombre2: 'UCE',
+        direccion2: 'America',
+        rector2: {
+            nombreR2: 'Daniel',
+            apellidoR2: 'Teran'
+        }
+    };
+
+    const { nombre2, rector2: { nombreR2 } } = universidad2;
+    console.log('NOMBRER2: ', nombreR2);
+
+    // Desestructuración de objetos por operador rest
+    console.log('>> Desestructuración de objeto por operador rest');
+
+    const autoR = {
+        marcaR: 'Toyota',
+        modeloR: 'Prius',
+        anioR: 2024,
+        colorR: 'Rojo',
+    }
+
+    const { marcaR, colorR, ...restoO } = autoR;
+    console.log(marcaR)
+    console.log(colorR)
+    console.log(restoO)
 
 };
+
+function desestructuracionArreglo([c1, c2, c3]) {
+    console.log('COLOR ->', c1);
+    console.log('COLOR ->', c2);
+    console.log('COLOR ->', c3);
+}
+
+function desestructuracionArreglo2() {
+    const colores = ['Rosado', 'Lila', 'Dorado'];
+    return colores;
+}
+
+function desestructuracionObjeto({ marca, color }) {
+    console.log('AUTO', marca);
+    console.log('AUTO', color);
+}
