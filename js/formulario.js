904fb80509113c59fd24fb5474af02e8b8db844f
kaandradec <kaandradec@gmail.com>
2025-04-30 19:42:09 -0500

    taller 9: Formulario

---
+++ js/formulario.js
@@ -0,0 +1,15 @@
+function guardar() {
+    validar()
+}
+
+function validar() {
+
+    let nombre = document.getElementById("id_nombre").ariaValueMax;
+
+    if (nombre === "") {
+        console.log('Error: No se ha ingresado el nombre');
+    } else {
+        console.log("Pasó la validación del nombre");
+    }
+
+}
