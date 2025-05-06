f7bcb9e95b1f08c4f74f07d43465f1e8dd05bc3e
Kevin Andrade <130163890+kaandradec@users.noreply.github.com>
2025-05-06 18:21:24 -0500

    tarea: completar validaciones de formulario

--- js/formulario.js
+++ js/formulario.js
@@ -2,14 +2,36 @@ function guardar() {
     validar()
 }
 
+const getElements = () => ({
+    nombreEl: document.getElementById('id_nombre'),
+    apellidoEl: document.getElementById('id_apellido'),
+    fechaEl: document.getElementById('id_fecha'),
+    emailEl: document.getElementById('id_email'),
+    passwordEl: document.getElementById('id_password'),
+    mensajeEl: document.getElementById('id_mensaje')
+})
+
 function validar() {
 
-    let nombre = document.getElementById("id_nombre").value;
+    const { nombreEl, apellidoEl, fechaEl, emailEl, passwordEl, mensajeEl } = getElements();
 
-    if (nombre === "") {
-        console.log('Error: No se ha ingresado el nombre');
+    // Color en caso de error en inputs
+    mensajeEl.classList.add('mensaje-error')
+
+    if (nombreEl.value === '') {
+        mensajeEl.innerText = 'El campo "nombre" es obligatorio'
+    } else if (apellidoEl.value === '') {
+        mensajeEl.innerText = 'El campo "apellido" es obligatorio'
+    } else if (fechaEl.value === '') {
+        mensajeEl.innerText = 'El campo "fecha de nacimiento" es obligatorio'
+    } else if (emailEl.value === '') {
+        mensajeEl.innerText = 'El campo "email" es obligatorio'
+    } else if (passwordEl.value === '') {
+        mensajeEl.innerText = 'El campo "password" es obligatorio'
     } else {
-        console.log("Pasó la validación del nombre");
+        mensajeEl.classList.remove('mensaje-error')
+        mensajeEl.classList.add('mensaje-exito')
+        mensajeEl.innerText = 'Formulario guardado con éxito';
     }
 
 }
